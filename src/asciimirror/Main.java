970dfd4432d53e8c93c4e902b2a42d7afb9726d6
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-01-24 21:19:29 +0100

    the cow is repeating the input content!

--- src/asciimirror/Main.java
+++ src/asciimirror/Main.java
@@ -3,20 +3,20 @@ package asciimirror;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Input the file path:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
         List<String> list = new ArrayList<>(Arrays.asList(
-                "                    _______ ",
-                "                   < hello >",
-                "                    ------- ",
-                "            ^__^   /        ",
-                "    _______/(oo)  /         ",
-                "/\\/(       /(__)            ",
-                "   | w----||                ",
-                "   ||     ||                "
-        ));
+                "            ^__^",
+                "    _______/(oo)",
+                "/\\/(       /(__)",
+                "   | w----||     ",
+                "   ||     ||     "));
+        System.out.println(input);
         for (String variable : list
         ) {
             System.out.println(variable);
